94f9c8a9175af1dfe24bfca59abc62ae740d22ca
mohamed-hadjadji <55872437+mohamed-hadjadji@users.noreply.github.com>
2020-02-07 15:32:55 +0100

    recommencer job1

--- user.php
+++ user.php
@@ -1,129 +1,54 @@
 <?php
-
-class User
+session_start();
+class user
 {
-  
-	private $id = "";
-	public $login ="";
-	public $email ="";
-	public $firstname ="";
-	public $lastname ="";
+	private	$id ;
+	public $login;
+	private	$password;
+	public $email;
+	public $firstname;
+	public $lastname;
 
-	public function register($login,$password, $email, $firstname, $lastname)
+public function register($login,$password, $email, $firstname, $lastname)
 	{
-	  $connexion =  mysqli_connect("localhost","root","","classes");
-       if (isset($_POST['connexion']))
-       {
-            $login = $_POST['login'];
-	        $password= password_hash($_POST["mdp1"], PASSWORD_DEFAULT);
-	        $email= $_POST['email'];
-	        $firstname= $_POST['firstn'];
-	        $lastname= $_POST['lastn'];
+		 $connexion =  mysqli_connect("localhost","root","","classes");
+		 $sql = "INSERT INTO utilisateurs (login,password,email,firstname,lastname)
+                VALUES ('".$login."', '".$password."','".$email."','".$firstname."','".$lastname."')";
+         $query=mysqli_query($connexion,$sql);
 
-	        if ($_POST['mdp1']==$_POST['mdp2'])
-            {
-            $requet="SELECT* FROM utilisateurs WHERE login='".$login."'";
-            $query2=mysqli_query($connexion,$requet);
-            $resultat=mysqli_fetch_all($query2);
-            $trouve=false;
-            foreach ($resultat as $key => $value) 
-            {
-            if ($resultat[$key][1]==$_POST['login'])
-            {
-               $trouve=true;
-               echo "<p class='erreur'><b>Login déja existant!!</b></p>";
-            }
-           }
-           if ($trouve==false)
-           {
-            $sql = "INSERT INTO utilisateurs (login,password,email,firstname,lastname)
-                VALUES ('$login','$password', '$email','$firstname', '$lastname')";
-            $query=mysqli_query($connexion,$sql);
-            header('location:index.php');
-            }
-           }
-           else
-           {
-              echo "<p class='erreur'><b>Les mots de passe doivent être identique!</b></p>";
-           }
-        }
-
+         $table = 
+		array($login,$password,$firstname,$lastname,$email);
+    	return $table;
 	}
 
 	public function connect($login, $password)
 	{
-      $connexion =  mysqli_connect("localhost","root","","classes"); 
-      $login = htmlspecialchars($_POST['login']);
-      $password = htmlspecialchars($_POST['password']);
+        $connexion =  mysqli_connect("localhost","root","","classes");
+        $requete = "SELECT * FROM utilisateurs WHERE login='".$login."'";
+        $query=mysqli_query($connexion,$requete);
+		$resultat=mysqli_fetch_all($query);
+		if ($login==$resultat[0][1] && $password==$resultat[0][2])
+		{
+			$this->id = $resultat[0][0];
+			$this->login = $login;
+			$this->password = $password;
+			$this->email =$resultat[0][3];;
+		    $this->firstname= $resultat[0][4];
+		    $this->lastname= $resultat[0][5];
 
-            if($login !== "" && $password !== "")
-            {
-                $requete = "SELECT count(*) FROM utilisateurs where
-                login = '".$login."' ";
-                $exec_requete = mysqli_query($connexion,$requete);
-                $reponse      = mysqli_fetch_array($exec_requete);
-                $count = $reponse['count(*)'];
-
-                $requete4 = "SELECT * FROM utilisateurs WHERE login='".$login."'";
-                $exec_requete4 = mysqli_query($connexion,$requete4);
-                $reponse4 = mysqli_fetch_array($exec_requete4);
-
-                if( $count!=0 && $_SESSION['login'] !== "" && password_verify($password, $reponse4[2]) )
-                {
-            
-                $_SESSION['login'] = $_POST['login'];
-                $user = $_SESSION['login'];
-
-            echo "<h3><b>Bonjour <u>$user,</u> vous êtes connecté</b></h3>";
-
-                }
-                
-            }
-	}
-
-	public function disconnect()
-	{
-      session_unset();
-                      
-	}
-
-	public function delete()
-	{
-		$connexion =  mysqli_connect("localhost","root","","classes"); 
-		$requete = "DELETE FROM utilisateurs WHERE login = '".$_SESSION['login']."'";
-		$query=mysqli_query($connexion,$requete);
-
-
-	}
-
-	public function update($login, $password, $email, $firstname,$lastname)
-	{
-         $connexion = mysqli_connect ("localhost", "root", "", "classes");
-          $password= password_hash($_POST["password"],PASSWORD_DEFAULT);
-         $requete = "UPDATE utilisateurs SET login = '$login', password = '$password', email = '$email', firstname = '$firstname', lastname = '$lastname' WHERE login = '".$_SESSION['login']."'";
-      
-         $query = mysqli_query($connexion,$requete); 
-         
-         $_SESSION['login'] = $login;
-         header('Location: index.php');
-     }
-
-     public function isConnected()
-     {
-     	if (isset($_SESSION['login'])==false)
-       {
-       echo "<h3>Vous n'êtes pas Connecté !!";
-       }
-       elseif(isset($_SESSION['login'])==true)
-
-       {
-      
-        $user = $_SESSION['login'];
-            echo "<h3><b>Bonjour <u>$user,</u> vous êtes connecté.</b></h3>"; 
-       }
-    
-     }
-	
+			echo"Vous etes connecté";
+		}
+		else
+		{
+			echo"login ou mot de passe incorrect";
+		}
+    }
 }
 
-?>
+
+		$momo = new user();
+
+
+//$momo->register("mo","moma","moma@moma","MOMA","MO");
+$momo->connect("mo", "moma"); 
+//var_dump($momo);   		
